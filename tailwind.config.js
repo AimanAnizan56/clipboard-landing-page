78fdfe1f5b9c077adcf48cc450e8b50a8229e7ae
Aiman Anizan <anizanaimann1@gmail.com>
2023-08-13 20:55:21 +0800

    Add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,12 @@ export default {
       fontFamily: {
         BaiJamjuree: ['Bai Jamjuree', 'sans-serif'],
       },
+      colors: {
+        'primary-cyan': 'hsl(var(--color-strong-cyan) / <alpha-value>)',
+        'primary-blue': 'hsl(var(--color-light-blue) / <alpha-value>)',
+        'neutral-dark-gray-blue': 'hsl(var(--color-dark-grayish-blue) / <alpha-value>)',
+        'neutral-gray-blue': 'hsl(var(--color-grayish-blue) / <alpha-value>)',
+      },
     },
   },
   plugins: [],
