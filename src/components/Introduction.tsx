2ad2b9813debf51807b4c1dc101380b83b2f5403
Aiman Anizan <anizanaimann1@gmail.com>
2023-08-14 16:08:50 +0800

    Remove font bold

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -6,7 +6,7 @@ const Introduction = () => {
 
       <h1 className="font-bold text-3xl text-center w-[85vw] mx-auto mb-4">A history of everything you copy</h1>
 
-      <div className="font-bold text-center text-neutral-gray-blue w-[85vw] max-w-xl mx-auto mb-10">Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all your devices.</div>
+      <div className="text-center text-neutral-gray-blue w-[85vw] max-w-xl mx-auto mb-10">Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all your devices.</div>
 
       <div className="flex flex-col sm:flex-row justify-center gap-4">
         <button className="bg-primary-cyan active:bg-opacity-80 mx-10 md:mx-0 min-w-[200px] px-8 py-3 rounded-full text-white">Download for Mac</button>
