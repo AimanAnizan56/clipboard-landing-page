d9db90e3f08e69cd6f12f367af6639a107c44795
Aiman Anizan <anizanaimann1@gmail.com>
2023-08-14 17:43:50 +0800

    Add z-index

--- src/components/BackgroundImage.tsx
+++ src/components/BackgroundImage.tsx
@@ -3,7 +3,7 @@ import bgMobile from '../assets/bg-header-mobile.png';
 
 const BackgroundImage = () => {
   return (
-    <div className="absolute top-0 left-0">
+    <div className="absolute top-0 left-0 z-0">
       <img src={bgDesktop} className="hidden md:block w-[100vw] h-20vh" />
       <img src={bgMobile} className="md:hidden w-[100vw] h-20vh" />
     </div>
--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -1,7 +1,7 @@
 import Logo from '../assets/logo.svg';
 const Introduction = () => {
   return (
-    <>
+    <div className="z-50 relative">
       <img src={Logo} className="w-28 mx-auto mt-32 mb-12" />
 
       <h1 className="font-bold text-4xl text-center w-[90vw] mx-auto mb-4">A history of everything you copy</h1>
@@ -9,10 +9,14 @@ const Introduction = () => {
       <div className="text-center text-neutral-gray-blue w-[85vw] max-w-[650px] mx-auto mb-10">Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all your devices.</div>
 
       <div className="flex flex-col sm:flex-row justify-center gap-4">
-        <button className="bg-primary-cyan active:bg-opacity-80 mx-10 md:mx-0 min-w-[200px] px-8 py-3 rounded-full text-white">Download for Mac</button>
-        <button className="bg-primary-blue active:bg-opacity-80 mx-10 md:mx-0 min-w-[200px] px-8 py-3 rounded-full text-white">Download for iOS</button>
+        <button type="button" className="cursor-pointer bg-primary-cyan active:bg-opacity-80 mx-10 md:mx-0 min-w-[200px] px-8 py-3 rounded-full text-white">
+          Download for Mac
+        </button>
+        <button type="button" className="cursor-pointer bg-primary-blue active:bg-opacity-80 mx-10 md:mx-0 min-w-[200px] px-8 py-3 rounded-full text-white">
+          Download for iOS
+        </button>
       </div>
-    </>
+    </div>
   );
 };
 
