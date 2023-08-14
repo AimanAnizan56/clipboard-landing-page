2ef80fc5de250ca638a689a5388d74187fe700b0
Aiman Anizan <anizanaimann1@gmail.com>
2023-08-14 17:38:16 +0800

    Change width and max width

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -4,9 +4,9 @@ const Introduction = () => {
     <>
       <img src={Logo} className="w-28 mx-auto mt-32 mb-12" />
 
-      <h1 className="font-bold text-3xl text-center w-[85vw] mx-auto mb-4">A history of everything you copy</h1>
+      <h1 className="font-bold text-4xl text-center w-[90vw] mx-auto mb-4">A history of everything you copy</h1>
 
-      <div className="text-center text-neutral-gray-blue w-[85vw] max-w-xl mx-auto mb-10">Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all your devices.</div>
+      <div className="text-center text-neutral-gray-blue w-[85vw] max-w-[650px] mx-auto mb-10">Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all your devices.</div>
 
       <div className="flex flex-col sm:flex-row justify-center gap-4">
         <button className="bg-primary-cyan active:bg-opacity-80 mx-10 md:mx-0 min-w-[200px] px-8 py-3 rounded-full text-white">Download for Mac</button>
