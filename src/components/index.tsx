518968d6e3d8d65bfa1fdcf3b8130d87526ae4d0
Aiman Anizan <anizanaimann1@gmail.com>
2023-08-17 14:41:31 +0800

    Rename file

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,6 @@
 import Introduction from './Introduction';
 import BackgroundImage from './BackgroundImage';
 import Feature from './Feature';
-import DevicesAccess from './Access';
+import DevicesAccess from './DevicesAccess';
 
 export { Introduction, BackgroundImage, Feature, DevicesAccess };
