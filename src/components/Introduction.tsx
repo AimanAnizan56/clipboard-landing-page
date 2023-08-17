67d840599e121957085d725ef8eae49207e9a75e
Aiman Anizan <anizanaimann1@gmail.com>
2023-08-17 15:40:13 +0800

    Seperate class into btn component

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -9,11 +9,11 @@ const Introduction = () => {
       <div className="text-center text-neutral-gray-blue w-[85vw] max-w-[650px] mx-auto mb-10">Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all your devices.</div>
 
       <div className="flex flex-col sm:flex-row justify-center gap-4">
-        <button type="button" className="cursor-pointer bg-primary-cyan active:bg-opacity-80 mx-10 sm:mx-0 min-w-[200px] px-8 py-3 rounded-full text-white">
-          Download for Mac
-        </button>
-        <button type="button" className="cursor-pointer bg-primary-blue active:bg-opacity-80 mx-10 sm:mx-0 min-w-[200px] px-8 py-3 rounded-full text-white">
+        <button type="button" className="btn-download-ios">
           Download for iOS
+        </button>
+        <button type="button" className="btn-download-mac">
+          Download for Mac
         </button>
       </div>
     </div>
