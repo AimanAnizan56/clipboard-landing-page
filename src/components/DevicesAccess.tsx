07dbd1899615dc8e584726dcda6dfbbe5eb784a5
Aiman Anizan <anizanaimann1@gmail.com>
2023-08-17 15:09:23 +0800

    Edit margin layout

--- src/components/DevicesAccess.tsx
+++ src/components/DevicesAccess.tsx
@@ -3,7 +3,7 @@ import imgDevices from '../assets/image-devices.png';
 const DevicesAccess = () => {
   return (
     <>
-      <div className="mb-10">
+      <div className="mb-20">
         <h3 className="font-bold text-3xl text-center w-[75vw] mx-auto mt-32 mb-6">Access Clipboard Anywhere</h3>
 
         <p className="text-neutral-gray-blue text-center w-[88vw] max-w-[700px] mx-auto">Whether you’re on the go, or at your computer, you can access all your Clipboard snippets in a few simple clicks.</p>
